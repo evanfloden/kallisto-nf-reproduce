e59e26a43ab8b2585c63a39b836e3aab1a65f23c
skptic <evanfloden@gmail.com>
2016-03-22 09:18:00 +0100

    Added mac results

--- kallisto-mac/launch.sh
+++ kallisto-mac/launch.sh
@@ -1,7 +1,7 @@
 mkdir -p ./mac/kallisto/
 ./kallisto-std.sh \
-    ../data/raw_reads \
-    ../data/transcriptome/Homo_sapiens.GRCh38.rel79.cdna.all.fa  \
-    ../data/experiment/hiseq_info.txt \
+    /Volumes/ExternalDrive2/temp/nextflow-kallisto-publication/data/raw_reads \
+    /Volumes/ExternalDrive2/temp/nextflow-kallisto-publication/data/transcriptome/Homo_sapiens.GRCh38.rel79.cdna.all.fa  \
+    /Volumes/ExternalDrive2/temp/nextflow-kallisto-publication/data/experiment/hiseq_info.txt \
     kallisto_mac-v0.42.4.bin \
     mac
