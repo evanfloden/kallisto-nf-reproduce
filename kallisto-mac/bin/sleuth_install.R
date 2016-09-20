06e1db4775bfdc4153666e931069531fe8776e2d
skptic <evanfloden@gmail.com>
2016-09-20 14:31:19 +0200

    Added versions to ensembl annotation mart

--- kallisto-mac/bin/sleuth_install.R
+++ kallisto-mac/bin/sleuth_install.R
@@ -1,8 +1,5 @@
-!#/usr/bin/env Rscript
-
+#!/usr/bin/env Rscript
 source("http://bioconductor.org/biocLite.R")
 biocLite("rhdf5")
-
-install.packages("devtools")
-
-devtools::install_github("pachterlab/sleuth")
+install.packages("devtools", repos='http://cran.us.r-project.org')
+devtools::install_github("pachterlab/sleuth", ref = "v0.28.0" )
