6a90f872730bedadeb7e7f9adac6889e76949fe8
skptic <evanfloden@gmail.com>
2016-09-21 09:56:22 +0200

    Clean up

--- kallisto-linux/sleuth_install.R
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env Rscript
-source("http://bioconductor.org/biocLite.R")
-biocLite("rhdf5")
-install.packages("devtools", repos='http://cran.us.r-project.org')
-devtools::install_github("pachterlab/sleuth", ref = "v0.28.0" )
